39e6a1e135d26e7ce89ed9efe30896e997e98423
ariel <arielgenua24@gmail.com>
2025-01-14 16:30:33 -0300

    added a new folder and a .jsx to separate the modal logic from inventory logic

--- src/pages/Inventory/index.jsx
+++ src/pages/Inventory/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import useFirestoreContext from '../../hooks/useFirestoreContext';
+import ProductFormModal from '../../modals/ProductFormModal';
 import './styles.css';
 
 const Inventory = () => {
@@ -29,6 +30,7 @@ const Inventory = () => {
     setIsModalOpen(false);
     const updatedProducts = await getProducts();
     setProducts(updatedProducts);
+    //reset newProduct state
     setNewProduct({ name: '', price: '', size: '', color: '', stock: '' });
   };
 
@@ -63,42 +65,7 @@ const Inventory = () => {
       </button>
 
       {isModalOpen && (
-        <div className="modal">
-          <form onSubmit={handleSubmit} className="modalContent">
-            <h3 className="subtitle">Nuevo Producto</h3>
-            
-            {['name', 'price', 'size', 'color', 'stock'].map(field => (
-              <div key={field} className="formGroup">
-                <label className="label">
-                  {field === 'name' ? 'Nombre del producto' : field}
-                </label>
-                <input
-                  type={field === 'price' || field === 'stock' ? 'number' : 'text'}
-                  value={newProduct[field]}
-                  onChange={(e) => setNewProduct({
-                    ...newProduct,
-                    [field]: e.target.value
-                  })}
-                  className="input"
-                  required
-                />
-              </div>
-            ))}
-
-            <div className="buttonGroup">
-              <button type="submit" className="button">
-                Guardar
-              </button>
-              <button
-                type="button"
-                onClick={() => setIsModalOpen(false)}
-                className="button"
-              >
-                Salir
-              </button>
-            </div>
-          </form>
-        </div>
+        <ProductFormModal handleSubmit={handleSubmit} newProduct={newProduct} setNewProduct={setNewProduct} setIsModalOpen={setIsModalOpen}/>
       )}
     </div>
   );
