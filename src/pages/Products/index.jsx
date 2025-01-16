1d85e993264f84c3e8169b9a29da71d69f28efb2
ariel <arielgenua24@gmail.com>
2025-01-16 09:53:49 -0300

    changed the button -save item- to enhance the ux allowing users to understand if they made a change or not

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFirestoreContext from '../../hooks/useFirestoreContext';
 import './styles.css';
 
 function Product() {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const [product, setProduct] = useState({});
-  
+  const [changes, setChanges] = useState(null);
 
   //fetching the product
   const { getProduct } = useFirestoreContext();
@@ -34,6 +35,10 @@ function Product() {
       loadProducts();
     }, [getProduct, id]);
 
+    const handleInputChange = (setter) => (e) => {
+      setter(e.target.value); // Actualiza el valor del campo
+      setChanges(true);       // Marca que hay cambios pendientes
+    };
   
 
     const handleSubmit = (e) => {
@@ -48,6 +53,7 @@ function Product() {
       };
       console.log("Product updated:", updatedProduct);
       // Add code here to save the updated product (e.g., send it to a backend)
+      navigate('/inventory')
     };
 
   return (
@@ -55,45 +61,67 @@ function Product() {
       <h1 className="product-title">Producto</h1>
       <form className="product-card" onSubmit={handleSubmit}>
       <div className="product-card">
-        <input
-          type="text"
-          className="product-input"
-          placeholder={`Nombre: ${product.name}`}
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+        <div className="input-group">
+          <span>Nombre</span>
+          <input
+            type="text"
+            className="product-input"
+            placeholder={`Nombre: ${product.name}`}
+            value={name}
+            onChange={handleInputChange(setName)}
+          />
+        </div>
+      <div className="input-group">
+        <span>Precio</span>
         <input
           type="number"
           className="product-input"
           placeholder={`Precio: ${product.price}`}
           value={price}
-          onChange={(e) => setPrice(e.target.value)}
+          onChange={handleInputChange(setPrice)}
         />
+      </div>
+      <div className="input-group">
+        <span>Cantidad total en stock</span>
         <input
           type="number"
           className="product-input"
           placeholder={`Stock: ${product.stock}`}
           value={stock}
-          onChange={(e) => setStock(e.target.value)}
+          onChange={handleInputChange(setStock)}
         />
+      </div>
+      <div className="input-group">
+        <span>Talle</span>
         <input
           type="text"
           className="product-input"
           placeholder={`Talle: ${product.size}`}
           value={size}
-          onChange={(e) => setSize(e.target.value)}
+          onChange={handleInputChange(setSize)}
         />
+      </div>
+      <div className="input-group">
+        <span>Color</span>
         <input
           type="text"
           className="product-input"
           placeholder={`Color: ${product.color}`}
           value={color}
-          onChange={(e) => setColor(e.target.value)}
+          onChange={handleInputChange(setColor)}
         />
+      </div>
 
+      {changes ? (
         <button type="submit" className="submit-button">
           Guardar Cambios
         </button>
+      ) : (
+        <button className="go-back" onClick={() => navigate('/inventory')}>
+          Volver al Inventario
+        </button>
+      )}
+       
 
       </div>
 
