2b420bbfa6ea786c4d5e3f1d4db4f820bf7931f8
ariel <arielgenua24@gmail.com>
2025-02-01 20:03:21 -0300

    added a better ui to the login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,25 +1,27 @@
 import { useState } from "react";
-import {login} from "../../services/authService";
+import { login } from "../../services/authService";
 import useFirestoreContext from "../../hooks/useFirestoreContext";
 import { useNavigate } from "react-router-dom";
+import "./styles.css";
+import { Eye, EyeOff } from "lucide-react";
 
 const Login = () => {
- const navigate = useNavigate();
-  const {user, setUser} = useFirestoreContext()
+  const navigate = useNavigate();
+  const { setUser } = useFirestoreContext();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-        console.log(email, password)
       const newUser = await login(email, password);
       alert("Inicio de sesión exitoso");
       setUser(email);
-      if(newUser) {
-        navigate(-2)
+      if (newUser) {
+        navigate(-2);
       }
     } catch (err) {
       setError(err.message);
@@ -27,14 +29,35 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Iniciar Sesión</h2>
-      <form onSubmit={handleLogin}>
-        <input type="email" placeholder="Correo" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button type="submit">Iniciar Sesión</button>
+    <div className="login-container">
+      <h2 className="login-title">Iniciar Sesión</h2>
+      <form className="login-form" onSubmit={handleLogin}>
+        <input
+          className="login-input"
+          type="email"
+          placeholder="Correo"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <div className="login-password-container">
+          <input
+            className="login-input"
+            type={showPassword ? "text" : "password"}
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          >  </input>
+         <button
+          type="button"
+          className="login-password-toggle"
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          {showPassword ? <EyeOff size={20} color="#0E93FF" /> : <Eye size={20} color="#0E93FF" />}
+        </button>
+        </div>
+        <button className="login-button" type="submit">Iniciar Sesión</button>
       </form>
-      {error && <p>{error}</p>}
+      {error && <p className="login-error">{error}</p>}
     </div>
   );
 };
