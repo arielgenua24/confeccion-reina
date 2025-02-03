e4259b8533f94f75e98b42627e1c0993cc5b1382
ariel <arielgenua24@gmail.com>
2025-02-03 20:10:19 -0300

    fixed some problems with qr to pdfs

--- src/modals/Qrmodal/index.jsx
+++ src/modals/Qrmodal/index.jsx
@@ -1,5 +1,6 @@
 import { QRCodeCanvas } from 'qrcode.react';
 import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 import './styles.css';
 
 function QRmodal({ QRcode, setQRcode, orderCode }) {
@@ -18,11 +19,28 @@ function QRmodal({ QRcode, setQRcode, orderCode }) {
     const canvas = document.querySelectorAll('.qr-canvas');
     let yPos = 10;
     
-    canvas.forEach((c) => {
+    canvas.forEach((c, index) => {
+      // Configurar estilo de borde
+      pdf.setDrawColor(200); // Color gris claro para el borde
+      pdf.setLineWidth(0.5);
+      pdf.rect(10, yPos, 190, 80); // Dibujar rectángulo que contendrá el elemento
+      
+      // Añadir texto
+      const detailText = orderCode
+        ? `Pedido de: ${QRcode.cliente} Código: ${QRcode.orderCode}`
+        : `Producto: ${QRcode.name} Código: ${QRcode.productCode}`;
+      
+      pdf.setFontSize(10);
+      pdf.text(15, yPos + 10, detailText);
+      
+      // Añadir QR code debajo del texto
       const imgData = c.toDataURL('image/png');
-      pdf.addImage(imgData, 'PNG', 10, yPos, 50, 50);
-      yPos += 60;
+      pdf.addImage(imgData, 'PNG', 80, yPos + 15, 50, 50);
+      
+      // Aumentar posición vertical para el siguiente elemento
+      yPos += 80;
     });
+    
     pdf.save('QR-codes.pdf');
   };
 
@@ -30,7 +48,7 @@ function QRmodal({ QRcode, setQRcode, orderCode }) {
     <div className="QR-modalOverlay">
       <div className="QR-modalContent">
         <div className="QR-container">
-          {[...Array(5)].map((_, index) => (
+          {[...Array(3)].map((_, index) => (
             <div key={index} className="QR-item">
               <h4 className="QR-title">
                 {orderCode
