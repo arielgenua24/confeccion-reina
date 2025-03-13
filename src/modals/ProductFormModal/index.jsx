36b1e0c6d47cd4546741200d0791fd6c6bb4507b
ariel <arielgenua24@gmail.com>
2025-03-13 19:42:15 -0300

    added a fn to suggest name, color, items, price

--- src/modals/ProductFormModal/index.jsx
+++ src/modals/ProductFormModal/index.jsx
@@ -1,18 +1,109 @@
+import { useState, useEffect } from 'react'
+import useFirestoreContext from '../../hooks/useFirestoreContext'
+import LoadingComponent from '../../components/Loading'
+import showSuggestionNotification from '../../utils/showSuggestionNotification'
+import searchProducts from '../../utils/searchFn'
 import './styles.css'
 
 // eslint-disable-next-line react/prop-types
 function ProductFormModal({handleSubmit, newProduct, setNewProduct, setIsModalOpen}){
+  const [suggestions, setSuggestions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+  const [products, setProducts] = useState([]);
+
+  const {getProducts} = useFirestoreContext();
+
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      setIsLoading(true)
+      const fetchedProducts = await getProducts();
+      setProducts(fetchedProducts);
+      console.log(fetchedProducts);
+      setIsLoading(false)
+    };
+    loadProducts();
+  }, []);
+
+
+  const handleNameChange = async (e) => {
+    const value = e.target.value;
+    console.log(value)
+    setNewProduct({ ...newProduct, name: value });
+    
+    // Realizamos la búsqueda cuando el texto tiene al menos 3 caracteres
+    if (value.length >= 3) {
+      console.log('working search')
+      try {
+        const results = await searchProducts(products, value);
+        setSuggestions(results);
+      } catch (error) {
+        console.error("Error al buscar productos:", error);
+        setSuggestions([]);
+      }
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const handleSuggestionClick = (suggestion) => {
+    // Al hacer click en la sugerencia, se completan los datos deseados
+    setNewProduct({
+      ...newProduct,
+      name: suggestion.name,
+      price: suggestion.price,
+      color: suggestion.color,
+      category: suggestion.category,
+    });
+     // setImages({image1: suggestion.image1, image2: suggestion.image2, image3: suggestion.image3});
+    setSuggestions([]);
+    // Mostrar la notificación
+    showSuggestionNotification();
+  };
 
     return(
         <div className="modal">
+
+        {isLoading && LoadingComponent}
         <form onSubmit={handleSubmit} className="modalContent">
           <h3 className="subtitle">Nuevo Producto</h3>
           
-          {['name', 'price', 'size', 'color', 'stock'].map(field => (
+          <div className="formGroup">
+            <label className="label">Nombre del producto</label>
+            <input
+              type="text"
+              //this in is broken after chosing a recommendation, it doesn't work after chosing a product
+              value={newProduct.name}
+              onChange={handleNameChange}
+              className="input"
+            />
+            {suggestions.length > 0 && (
+              <div className="suggestion-input--container">
+
+                <ul className="suggestion-input--list">
+                      {suggestions.map((suggestion, index) => (
+                        <li 
+                          key={index} 
+                          onClick={() => handleSuggestionClick(suggestion)}
+                          className="suggestion-input--item"
+                        >
+                          <span className="suggestion-input--name">{suggestion.name}</span>
+                          <span className="suggestion-input--color">{suggestion.color}</span>
+                          <span className="suggestion-input--price">${suggestion.price}</span>
+                        </li>
+                      ))}
+                    </ul>
+              </div>
+
+            )}
+          </div>
+
+
+
+          {['price', 'size', 'color', 'stock'].map(field => (
             <div key={field} className="formGroup">
               <label className="label">
-                {field === 'name' ? 'Nombre del producto' : 
-                field === 'price' ? 'Precio' : 
+                {field === 'price' ? 'Precio' : 
                 field === 'size' ? 'Talle' : 
                 field === 'color' ? 'Color' : 
                 field === 'stock' ? 'Cantidad en inventario' : 
